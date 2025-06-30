72121a67c4a9b3d108f210c61141e853602029ca
chenQiWen <547847705@qq.com>
2025-07-01 04:21:08 +0800

    refactor(layout): update Analytics import from @vercel/analytics/react to @vercel/analytics/next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
